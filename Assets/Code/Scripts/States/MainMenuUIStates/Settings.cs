5df6bc62ffaacdae7367dbae5a914ffaf8c4b80b
Mark Warren <thofnas2@gmail.com>
2024-04-22 23:58:02 +0300

    Update

--- Assets/Code/Scripts/States/MainMenuUIStates/Settings.cs
+++ Assets/Code/Scripts/States/MainMenuUIStates/Settings.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UI;
 using UI.Elements;
-using UnityEngine;
 using UnityEngine.UIElements;
 using Utilities.Extensions;
 
@@ -11,13 +10,16 @@ namespace States.MainMenuUIStates
     public class Settings : BaseState
     {
         private static readonly List<string> s_radioButtonChoices = new() { "30", "60", "120" };
-        
+
+        private readonly MainMenuUIManager _mainMenuUIManager;
         private readonly GameSettings _gameSettings;
+        
 
         public Settings(MainMenuUIManager mainMenuUIManager, StateMachine.StateMachine stateMachine, StyleSheet styleSheet, GameSettings gameSettings) : base(mainMenuUIManager, stateMachine)
         {
             Root = CreateDocument(nameof(Settings), styleSheet);
 
+            _mainMenuUIManager = mainMenuUIManager;
             _gameSettings = gameSettings;
             
             GenerateView();
@@ -33,11 +35,24 @@ namespace States.MainMenuUIStates
             VisualElement settingsContainer = container.CreateChild("settings-container");
             VisualElement fpsContainer = settingsContainer.CreateChild("fps-container");
 
-            RadioButtonGroup radioButtonGroup = new("Frames Per Second", s_radioButtonChoices);
-            radioButtonGroup.RegisterValueChangedCallback(e => _gameSettings.SetFrameRate(int.Parse(s_radioButtonChoices[e.newValue])));
-            radioButtonGroup.Focus();
-            settingsContainer.Add(radioButtonGroup);
+
+            Label label = new("Frames Per Second");
+            fpsContainer.Add(label);
             
+            GroupBox fpsGroupBox = fpsContainer.CreateChild<GroupBox>("fps-groupbox");
+            
+            StyledRadioButton fps30RadioButton = new(_mainMenuUIManager.SelectedTheme.PlayerTheme, fpsGroupBox) { text = "30" };
+            fps30RadioButton.RegisterValueChangedCallback(_ => _gameSettings.SetFrameRate(30));
+            fpsGroupBox.Add(fps30RadioButton);
+            
+            StyledRadioButton fps60RadioButton = new(_mainMenuUIManager.SelectedTheme.PlayerTheme, fpsGroupBox) { text = "60" };
+            fps60RadioButton.RegisterValueChangedCallback(_ => _gameSettings.SetFrameRate(60));
+            fpsGroupBox.Add(fps60RadioButton);
+            
+            StyledRadioButton fps120RadioButton = new(_mainMenuUIManager.SelectedTheme.PlayerTheme, fpsGroupBox) { text = "120" };
+            fps120RadioButton.RegisterValueChangedCallback(_ => _gameSettings.SetFrameRate(120));
+            fpsGroupBox.Add(fps120RadioButton);
+
             VisualElement buttonsContainer = container.CreateChild("buttons-container");
             
             StyledButton backToMainMenuButton = new(SelectedTheme.PlayerTheme, buttonsContainer,
---
+++ Assets/Code/Scripts/UI/Elements/StyledRadioButton.cs
@@ -0,0 +1,33 @@
+﻿using Themes;
+using UnityEngine.UIElements;
+using Utilities.Extensions;
+
+namespace UI.Elements
+{
+    public class StyledRadioButton : RadioButton
+    {
+        private StyledRadioButton()
+        {
+        }
+        
+        public StyledRadioButton(Theme theme, VisualElement parent, params string[] wrapperClasses) : this(
+            theme, parent, null, wrapperClasses)
+        {
+        }
+        
+        public StyledRadioButton(Theme theme, VisualElement parent, string label,
+            params string[] wrapperClasses) :
+            base(label)
+        {
+            style.paddingBottom = 12;
+            style.paddingTop = 12;
+            style.paddingLeft = 16;
+            style.paddingRight = 16;
+
+            parent.Add(this);
+            this.AddClass(wrapperClasses);
+            style.unityBackgroundImageTintColor = theme.MainColor;
+            style.unityFontDefinition = new StyleFontDefinition(theme.BaseFont);
+        }
+    }
+}
